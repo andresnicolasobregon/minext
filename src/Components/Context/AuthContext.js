2cd23a391c89cd4634c6690dcf48f8a2f3856861
unknown <andresnicolasobregon@gmail.com>
2024-03-09 19:05:57 -0300

    link

--- src/Components/Context/AuthContext.js
+++ src/Components/Context/AuthContext.js
@@ -38,19 +38,11 @@ export const AuthProvider = ({ children }) => {
         onAuthStateChanged(auth, async (user) => {
 
             if (user) {
-                // const docRef = doc(db, "roles", user.uid)
-                // const userDoc = await getDoc(docRef)
-
-                // if (userDoc.data()?.rol === "admin") {
                     setUser({
                         logged: true,
                         email: user.email,
                         uid: user.uid
                     })
-                // } else {
-                //     router.push("/unauthorized")
-                //     logOut()
-                // }
             } else {
                 setUser({
                     logged: false,
--- src/app/admin/edit/[slug]/page.js
+++ src/app/admin/edit/[slug]/page.js
@@ -3,7 +3,7 @@ import EditForm from "@/Components/admin/EditForm"
 
 const EditPage = async ({ params }) => {
     const { slug } = params
-    const item = await fetch(`http://localhost:3000/api/product/${slug}`, {
+    const item = await fetch(`http:/${process.env.VERCEL_URL}/api/product/${slug}`, {
         cache: "no-store"
     }).then(res => res.json())
 
